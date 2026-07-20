05d0f7b7ce1a4bb7f4b5024beee3a771546c1b8f
Gabriel Alboneti <gabriel.alboneti@gmail.com>
2026-07-20 11:52:28 -0300

    Finished numbers exercise

--- Basic JS/exercicio_numbers/script.js
+++ Basic JS/exercicio_numbers/script.js
@@ -1,4 +1,18 @@
-const num = prompt("Digite um número:");
+const num = Number(prompt("Digite um número:")); // lembre que prompt sempre retorna uma string
+// const texto = document.getElementById("text");
+const num_class = document.getElementsByClassName("number");
 
-document.getElementById("title").innerText += ` ${num}`;
+document.getElementById("sqr-root").innerHTML = Math.sqrt(num);
+document.getElementById("bool-integer").innerHTML = Number.isInteger(num);
+document.getElementById("bool-nan").innerHTML = Number.isNaN(num);
+document.getElementById("round-floor").innerHTML = Math.floor(num);
+document.getElementById("round-up").innerHTML = Math.ceil(num);
+document.getElementById("two-decimals").innerHTML = num.toFixed(2);
+
+for (let i = 0; i < num_class.length; i++) {
+   const element = num_class[i];
+   element.innerHTML = `${num}`;
+}
+
+// DOM = Document Object Model -> representado pelo objeto document
 
