46c87993974907ff8b0d8c978e5f333b3f71c6f3
Gabriel Alboneti <gabriel.alboneti@gmail.com>
2026-07-31 12:08:16 -0300

    Finished logic operators lesson

--- Intermidiate JS/operadores_comparacao.js
+++ Intermidiate JS/operadores_comparacao.js
@@ -1,2 +1,16 @@
-let user = 1;
+/* 
+Operadores de comparação:
+- > maior que;
+- >= maior que ou igual;
+- < menor que;
+- <= menor que ou igual;
+- == igualdade (valor) -> não é recomendado. Sempre use a versão estrita;
+- != diferente (valor) -> não é recomendado. Sempre use a versão estrita;
+- === igualdade estrita (valor e tipo);
+- !== diferente estrito (valor e tipo);
+*/
+const num1 = 10;
+const num2 = "10";
+const comp = num1 !== num2;
+console.log(comp);
 
---
+++ Intermidiate JS/operadores_logicos.js
@@ -0,0 +1,14 @@
+/*
+Operadores Lógicos
+&& -> AND -> E
+|| ->OR -> OU
+! -> NOT -> NÃO
+*/
+const usuario = "Gabriel";
+const senha = "123456";
+
+const checagemLogin = usuario === "Gabriel" && senha === "12345";
+
+console.log(checagemLogin);
+console.log(!true, !false);
+
